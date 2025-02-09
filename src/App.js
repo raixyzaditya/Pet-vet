28884c1b15998a8dfae59bdc5ed78885233410db
ADITYA RAI <adi.hack1234@gmail.com>
2025-02-09 13:18:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Aboutus from "./pages/Aboutus";
-import Login from "./pages/Login"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 import Healthguide from "./pages/Healthguide";
 import Register from "./pages/Register";
 import Docreg from "./pages/Docreg";
@@ -9,13 +9,14 @@ import Bookdoc from "./pages/Bookdoc";
 import Appoint from "./pages/Appoint";
 import Doclogin from "./pages/Doclogin";
 import Docdash from "./pages/Docdash";
-import { useLocation } from "react-router-dom";
 
-function App() {
+function Layout() {
   const location = useLocation();
+  const hideNavbarRoutes = ["/doctor/dashboard"]; // Routes where Navbar should be hidden
+
   return (
-    <BrowserRouter>
-      {location.pathname !== "/doctor/dashboard" && <Navbar />}
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
 
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -28,8 +29,14 @@ function App() {
         <Route path="/Doclogin" element={<Doclogin />} />
         <Route path="/doctor/dashboard" element={<Docdash />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
+}
